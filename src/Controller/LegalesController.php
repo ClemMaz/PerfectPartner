03a2fc6442e8fe672528dc1884269ce42f20ec4d
ClemMaz <clementmazurkiewicz.dev@gmail.com>
2024-05-18 09:47:46 +0200

    Conditions légales MAJ

---
+++ src/Controller/LegalesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LegalesController extends AbstractController
+{
+    #[Route('/legales', name: 'app_legales')]
+    public function index(): Response
+    {
+        return $this->render('legales/index.html.twig', [
+            'controller_name' => 'LegalesController',
+        ]);
+    }
+}
